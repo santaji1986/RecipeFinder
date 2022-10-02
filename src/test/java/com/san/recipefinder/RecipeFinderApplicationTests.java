cc2e4dea094c5ab535e366d8cf1d8868de1d89ba
Santaji Suryawanshisantaji1986@gmail.com <Santaji Suryawanshisantaji1986@gmail.com>
2022-10-03 00:03:19 +0200

    Initialized the Recipe Finder Service

---
+++ src/test/java/com/san/recipefinder/RecipeFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.san.recipefinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
