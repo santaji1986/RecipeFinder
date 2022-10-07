5489360dc18fb9268f6f5f50ff9963dbd8a4ddac
Santaji Suryawanshi <santaji1986@gmail.com>
2022-10-07 21:04:22 +0200

    Removed test api call "hello" endpoint.

--- src/main/java/com/san/recipefinder/controller/RecipeController.java
+++ src/main/java/com/san/recipefinder/controller/RecipeController.java
@@ -15,44 +15,37 @@ import java.util.List;
 @RestController
 public class RecipeController {
     private static final Logger LOGGER = LoggerFactory.getLogger(RecipeController.class);
-    private RecipeService recipeService;
+    private final RecipeService recipeService;
 
     public RecipeController(RecipeService recipeService) {
         this.recipeService = recipeService;
-    }
-
-    @GetMapping(value = "/hello")
-    public String hello(@RequestParam(value = "name", required = false) String name) {
-        String greeting = "Hello " + name;
-        LOGGER.info(greeting);
-        return greeting;
     }
 
     @PutMapping(value = "/addRecipe")
     public ResponseEntity<String> addRecipe(@RequestBody AddRecipePayload addRecipePayload) {
         LOGGER.info("addRecipe {}", addRecipePayload);
         recipeService.addRecipe(addRecipePayload);
-        return new ResponseEntity<String>("Success", HttpStatus.CREATED);
+        return new ResponseEntity<>("Success", HttpStatus.CREATED);
     }
 
     @PostMapping(value = "/updateRecipe")
     public ResponseEntity<String> updateRecipe(@RequestBody UpdateRecipePayload updateRecipePayload) {
         LOGGER.info("updateRecipe {}", updateRecipePayload);
         recipeService.updateRecipe(updateRecipePayload);
-        return new ResponseEntity<String>("Success", HttpStatus.OK);
+        return new ResponseEntity<>("Success", HttpStatus.OK);
     }
 
     @DeleteMapping(value = "/deleteRecipe")
     public ResponseEntity<String> deleteRecipe(@RequestParam Long recipeId) {
         LOGGER.info(recipeId.toString());
         recipeService.deleteRecipe(recipeId);
-        return new ResponseEntity<String>("Success", HttpStatus.OK);
+        return new ResponseEntity<>("Success", HttpStatus.OK);
     }
 
     @GetMapping(value = "/getAllRecipes")
     public ResponseEntity<List<Recipe>> getAllRecipes() {
         LOGGER.info("Get All Recipes");
         List<Recipe> recipeList = recipeService.getAllRecipes();
-        return new ResponseEntity<List<Recipe>>(recipeList, HttpStatus.OK);
+        return new ResponseEntity<>(recipeList, HttpStatus.OK);
     }
 }
